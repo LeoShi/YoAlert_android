7a74aeea3cde563fe871016d1372616d30954a90
Shi Lei <mleoshi@gmail.com>
2013-07-28 22:15:00 +0800

    init project

---
+++ src/com/yoalert/cops/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.yoalert.cops;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void btn_login_click(View view){
+
+    }
+}
